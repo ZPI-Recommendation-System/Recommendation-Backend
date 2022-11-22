6436fe78a232f01b103d168eca4d2534b66c89dd
Z-Grate <supergamers.pl@gmail.com>
2022-11-22 17:08:29 +0100

    #33npkgp[WIP]

--- src/laptops/laptops.controller.spec.ts
+++ src/laptops/laptops.controller.spec.ts
@@ -1,28 +1,83 @@
-// import { Test, TestingModule } from "@nestjs/testing";
-// import { LaptopsController } from "./laptops.controller";
-// import { LaptopsServices } from "./laptops.service";
-//
-// describe("LaptopsController", () => {
-//   let laptopController: LaptopsController
-//
-//
-//
-//   beforeEach(async () => {
-//     const mockLaptopsService{
-//
-//     }
-//     const app: TestingModule = await Test.createTestingModule({
-//       imports: [TypeORM],
-//       controllers: [LaptopsController],
-//       providers: [LaptopsServices]
-//     }).compile();
-//
-//     appController = app.get<AppController>(AppController);
-//   });
-//
-//   describe("root", () => {
-//     it("should return \"Hello World!\"", () => {
-//       expect(appController.getHello()).toBe("Hello World!");
-//     });
-//   });
-// });
+import { Test, TestingModule } from "@nestjs/testing";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { LaptopsController } from "./laptops.controller";
+import { LaptopsServices } from "./laptops.service";
+import { ModelEntity } from "./entity/model.entity";
+import { ProcessorEntity } from "./entity/processor.entity";
+import { ScreenEntity } from "./entity/screen.entity";
+import { GraphicsEntity } from "./entity/graphics.entity";
+import { ConnectionEntity } from "./entity/connection.entity";
+import { ControlEntity } from "./entity/control.entity";
+import { MultimediaEntity } from "./entity/multimedia.entity";
+import { CommunicationEntity } from "./entity/communication.entity";
+import { BenchmarkEntity } from "./entity/benchmark.entity";
+import { ModelImgEntity } from "./entity/model-img.entity";
+import { DriveTypeEntity } from "./entity/drive-type.entity";
+import { Repository } from "typeorm";
+
+describe('LaptopsController', () => {
+  let laptopController: LaptopsController;
+  let module: TestingModule;
+
+
+  // const generateLaptops = (modelRepo: Repository<ModelEntity>) => {
+  //   const model: ModelEntity = {
+  //     id: randomStringGenerator(),
+  //     driveStorage: 1000,
+  //     driveType: "ssd",
+  //     batteryTime: 3,
+  //   };
+  //
+  //
+  // }
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          dropSchema: true,
+          synchronize: true,
+          entities: [
+            ModelEntity,
+            ProcessorEntity,
+            ScreenEntity,
+            GraphicsEntity,
+            ConnectionEntity,
+            ControlEntity,
+            MultimediaEntity,
+            CommunicationEntity,
+            BenchmarkEntity,
+            ModelImgEntity,
+            DriveTypeEntity,
+          ],
+        }),
+        TypeOrmModule.forFeature([
+          ModelEntity,
+          ProcessorEntity,
+          ScreenEntity,
+          GraphicsEntity,
+          ConnectionEntity,
+          ControlEntity,
+          MultimediaEntity,
+          CommunicationEntity,
+          BenchmarkEntity,
+          ModelImgEntity,
+          DriveTypeEntity,
+        ])
+      ],
+      providers: [LaptopsServices],
+    }).compile();
+
+    const moduleRepository: Repository<ModelEntity> = await module.get("ModelEntityRepository")
+
+    console.log(moduleRepository);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      // expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
