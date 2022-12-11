6a294d84f220dad5dde81025b51bfaab4f875685
Wojciech Bednarz <w.bednarz2000@gmail.com>
2022-12-11 16:13:54 +0100

    Refactoring

--- src/db/typeOrm.config.ts
+++ src/db/typeOrm.config.ts
@@ -1,17 +1,8 @@
-
 import { DataSource } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
-import { config } from 'dotenv';
 import process from "process";
-
-// config();
-//
-// const configService = new ConfigService();
 
 export default new DataSource({
   type: 'postgres',
-  // url: configService.get('DB_URL'),
-  // url: process.env.DB_URL,
   host: process.env.DB_HOST,
   port: +process.env.DB_PORT,
   username: process.env.DB_USER,
